1ba15ed9b70d25c7b85de7da8b5fe9db9aaff9ee
guillermooo <none@none>
2011-03-16 23:53:07 +0100

    Several improvements.
    --HG--
    branch : next

--- Lib/sublime_lib/path.py
+++ Lib/sublime_lib/path.py
@@ -3,6 +3,15 @@ import sublime
 import os
 
 
+FTYPE_EXT_KEYMAP        = "sublime-keymap"
+FTYPE_EXT_COMPLETIONS   = "sublime-completions"
+FTYPE_EXT_SNIPPET       = "sublime-snippet"
+FTYPE_EXT_BUILD         = "sublime-build"
+FTYPE_EXT_SETTINGS      = "sublime-settings"
+FTYPE_EXT_TMPREFERENCES = "tmPreferences"
+FTYPE_EXT_TMLANGUAGE    = "tmLanguage"
+
+
 def root_at_packages(*leafs):
     """Combines leafs with path to Sublime's Packages folder.
     """
