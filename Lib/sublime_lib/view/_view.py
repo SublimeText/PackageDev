bb5690a59d92e2d7f03f216638061950c5ca5f70
guillermooo <none@none>
2011-03-31 20:04:07 +0200

    Refactored sublime_lib.

--- Lib/sublime_lib/view/_view.py
+++ Lib/sublime_lib/view/_view.py
@@ -1,5 +1,11 @@
 
 
+def has_sels(view):
+    """Returns ``True`` if ``view`` has one selection or more.``
+    """
+    return len(view.sel()) > 0
+
+
 def has_file_ext(view, ext):
     """Returns ``True`` if view has file extension ``ext``.
     ``ext`` may be specified with or without leading ``.``.
--- tests/test_sublime_lib/test_view/test_sels.py
+++ tests/test_sublime_lib/test_view/test_sels.py
@@ -10,13 +10,3 @@ here = os.path.split(__file__)[0]
 path_to_lib = os.path.normpath(os.path.join(here, '..', '..', 'Lib'))
 if not path_to_lib in sys.path:
     sys.path.append(path_to_lib)
-
-
-import sublime_lib.view.sel as su_lib_sels
-
-
-def test_has_sels():
-    view = mock.Mock()
-    view.sel.return_value = range(1)
-
-    assert su_lib_sels.has_sels(view)
