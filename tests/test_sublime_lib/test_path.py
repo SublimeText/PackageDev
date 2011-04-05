e34975db01cf3d86c85837cab70f681d32909a52
guillermooo <guilan70@hotmail.com>
2011-04-05 23:47:23 +0200

    Revised test setup.
    --HG--
    branch : next

--- tests/test_sublime_lib/test_path.py
+++ tests/test_sublime_lib/test_path.py
@@ -5,13 +5,6 @@ import mock
 
 import sublime
 
-
-here = os.path.split(__file__)[0]
-path_to_lib = os.path.normpath(os.path.join(here, "..", "..", "Lib"))
-if not path_to_lib in sys.path:
-    sys.path.append(path_to_lib)
-
-
 import sublime_lib.path as su_path
 
 
