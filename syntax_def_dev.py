87949aa42190e4f867f27a1f8eb44537fcdaf20e
guillermooo <guilan70@hotmail.com>
2011-05-19 22:50:01 +0200

    Prettified code.

--- syntax_def_dev.py
+++ syntax_def_dev.py
@@ -4,19 +4,15 @@ import os
 import json2plist
 import uuid
 
-from sublime_lib.view import has_file_ext
-from sublime_lib.path import root_at_packages
+from sublime_lib.view import has_file_ext, in_one_edit
+from sublime_lib.path import root_at_data
 
 
-THIS_PACKAGE_NAME = "AAAPackageDev"
-THIS_PACKAGE_DEV_NAME = "XXX" + THIS_PACKAGE_NAME
-DEBUG = os.path.exists(sublime.packages_path() + "/" + THIS_PACKAGE_DEV_NAME)
-
-PATH_TO_JSON_TMLANGUAGE_SYNTAX_DEF = 'Packages/AAAPackageDev/Support//Sublime JSON Syntax Definition.tmLanguage'
-PATH_TO_SUBLIME_KEY_MAP_SYNTAX_DEF = 'Packages/AAAPackageDev/Support/Sublime Key Map.tmLanguage'
+JSON_TMLANGUAGE_SYNTAX = 'Packages/AAAPackageDev/Support/Sublime JSON Syntax Definition.tmLanguage'
+SUBLIME_KEY_MAP_SYNTAX = 'Packages/AAAPackageDev/Support/Sublime Key Map.tmLanguage'
 
 
-# TODO: insert template as snippet, replace name with lowercased name.
+# XXX: Move this to a txt file. Let user define his own under User too.
 def get_syntax_def_boilerplate():
     JSON_TEMPLATE = """{ "name": "${1:Syntax Name}",
   "scopeName": "source.${2:syntax_name}", 
@@ -27,49 +23,22 @@ def get_syntax_def_boilerplate():
 }"""
 
     actual_tmpl = JSON_TEMPLATE % str(uuid.uuid4())
-
     return actual_tmpl
-
-
-# Ugly name so Sublime normalizes in a sane way.
-class JsonToTmlanguage(sublime_plugin.TextCommand):
-    """Takes a syntax definition in JSON and converts it to a .tmLanguage
-    format (Plist) that Sublime Text understands.
-    """
-
-    def run(self, edit):
-      path, fname = os.path.split(self.view.file_name())
-      plist_grammar_name, ext = os.path.splitext(fname)
-
-      if not ext.lower() == ".json-tmlanguage":
-          sublime.error_message("~ AAAPackageDev Error ~\n\n"
-                                "Not a valid extension. (Must be: json-tmLanguage)")
-          return
-
-      try:
-          json2plist.make_grammar(self.view.file_name())
-      except IOError as e:
-          sublime.error_message(
-                        "[AAAPackageDev] Error while converting to Plist.")
-          return
 
 
 class NewSyntaxDefCommand(sublime_plugin.WindowCommand):
     """Creates a new syntax definition file for Sublime Text in JSON format
     with some boilerplate text.
     """
-
     def run(self):
-        # TODO: one_edit(view) context manager => Lib
         target = self.window.new_file()
-        edit = target.begin_edit()
-        target.settings().set("syntax", PATH_TO_JSON_TMLANGUAGE_SYNTAX_DEF)
-        target.run_command('insert_snippet', {
-                                                'contents':
-                                                get_syntax_def_boilerplate()}
-                                                )
-        # target.insert(edit, 0, get_syntax_def_boilerplate())
-        target.end_edit(edit)
+        
+        target.settings().set('default_dir', 'Packages/User')
+        target.settings().set('syntax', JSON_TMLANGUAGE_SYNTAX)
+        
+        with in_one_edit(target) as edit:            
+            target.run_command('insert_snippet',
+                               {'contents': get_syntax_def_boilerplate()})
 
 
 class NewSyntaxDefFromBufferCommand(sublime_plugin.TextCommand):
@@ -80,17 +49,20 @@ class NewSyntaxDefFromBufferCommand(sublime_plugin.TextCommand):
         return self.view.size() == 0
 
     def run(self, edit):
-        self.view.run_command('insert_snippet', {
-                                                'contents':
-                                                get_syntax_def_boilerplate()}
-                                                )
-        self.view.settings().set("syntax", PATH_TO_JSON_TMLANGUAGE_SYNTAX_DEF)
-        # TODO: force extension for saving. (Used to be: force_extension)
+        self.view.settings().set('default_dir', 'Packages/User')
+        self.view.settings().set('syntax', JSON_TMLANGUAGE_SYNTAX)
+
+        with in_one_edit(self.view):
+            self.view.run_command('insert_snippet',
+                                  {'contents': get_syntax_def_boilerplate()})
 
 
+# XXX: Why is this a WindowCommand? Wouldn't it work otherwise in build-systems?
 class MakeTmlanguageCommand(sublime_plugin.WindowCommand):
     """Generates a ``.tmLanguage`` file from a ``.JSON-tmLanguage`` syntax def.
+    Should be used from a ``.build-system only``.
     """
+    # XXX: We whould prevent this from working except if called in a build system.
     def is_enabled(self):
         return has_file_ext(self.window.active_view(), '.JSON-tmLanguage')
 
@@ -98,10 +70,11 @@ class MakeTmlanguageCommand(sublime_plugin.WindowCommand):
         v = self.window.active_view()
         path = v.file_name()
         if not (os.path.exists(path) and has_file_ext(v, 'JSON-tmLanguage')):
+            # XXX: Can we/Should we use sublimelog.log?
             print "[AAAPackageDev] Not a valid JSON-tmLanguage file. (%s)" % path
             return
 
-        build_cmd = root_at_packages("AAAPackageDev/Support/make_tmlanguage.py")
+        build_cmd = root_at_data("Packages/AAAPackageDev/Support/make_tmlanguage.py")
         file_regex = r"Error:\s+'(.*?)'\s+.*?\s+line\s+(\d+)\s+column\s+(\d+)"
         cmd = ["python", build_cmd, path]
 
