eb16c4272acfd737a2700ac2523748ec7aea28bf
guillermooo <guilan70@hotmail.com>
2012-03-28 22:24:48 +0200

    convert .JSON-tmLanguage to .tmLanguage with a ST command to avoid dependency on system's Python

--- syntax_def_dev.py
+++ syntax_def_dev.py
@@ -1,12 +1,13 @@
+import json
 import os
+import plistlib
+import sys
 import uuid
 
+import sublime_plugin
 
-import sublime, sublime_plugin
-
-
+from sublime_lib.path import root_at_packages
 from sublime_lib.view import has_file_ext, in_one_edit
-from sublime_lib.path import root_at_data, root_at_packages
 
 
 JSON_TMLANGUAGE_SYNTAX = 'Packages/AAAPackageDev/Support/Sublime JSON Syntax Definition.tmLanguage'
@@ -15,8 +16,8 @@ JSON_TMLANGUAGE_SYNTAX = 'Packages/AAAPackageDev/Support/Sublime JSON Syntax Def
 # XXX: Move this to a txt file. Let user define his own under User too.
 def get_syntax_def_boilerplate():
     JSON_TEMPLATE = """{ "name": "${1:Syntax Name}",
-  "scopeName": "source.${2:syntax_name}", 
-  "fileTypes": ["$3"], 
+  "scopeName": "source.${2:syntax_name}",
+  "fileTypes": ["$3"],
   "patterns": [$0
   ],
   "uuid": "%s"
@@ -32,10 +33,10 @@ class NewSyntaxDefCommand(sublime_plugin.WindowCommand):
     """
     def run(self):
         target = self.window.new_file()
-        
+
         target.settings().set('default_dir', root_at_packages('User'))
         target.settings().set('syntax', JSON_TMLANGUAGE_SYNTAX)
-        
+
         target.run_command('insert_snippet',
                            {'contents': get_syntax_def_boilerplate()})
 
@@ -73,8 +74,38 @@ class MakeTmlanguageCommand(sublime_plugin.WindowCommand):
             print "[AAAPackageDev] Not a valid JSON-tmLanguage file. (%s)" % path
             return
 
-        build_cmd = root_at_data("Packages/AAAPackageDev/Support/make_tmlanguage.py")
-        file_regex = r"Error:\s+'(.*?)'\s+.*?\s+line\s+(\d+)\s+column\s+(\d+)"
-        cmd = ["python", build_cmd, path]
+        assert os.path.exists(path), "Need a path to a .JSON-tmLanguage file."
+        self.make_tmlanguage_grammar(path)
+        # self.window.run_command("aaa_package_dev", {"cmd": cmd, "file_regex": file_regex})
 
-        self.window.run_command("exec", {"cmd": cmd, "file_regex": file_regex})
+    def make_tmlanguage_grammar(self, json_grammar):
+        path, fname = os.path.split(json_grammar)
+        grammar_name, ext = os.path.splitext(fname)
+        file_regex = r"Error:\s+'(.*?)'\s+.*?\s+line\s+(\d+)\s+column\s+(\d+)"
+
+        if not hasattr(self, 'output_view'):
+            # Try not to call get_output_panel until the regexes are assigned
+            self.output_view = self.window.get_output_panel("aaa_package_dev")
+
+        # FIXME: Can't get error navigation to work.
+        self.output_view.settings().set("result_file_regex", file_regex)
+        self.output_view.settings().set("result_base_dir", path)
+
+        # Call get_output_panel a second time after assigning the above
+        # settings, so that it'll be picked up as a result buffer
+        self.window.get_output_panel("aaa_package_dev")
+
+        with in_one_edit(self.output_view) as edit:
+            try:
+                with open(json_grammar) as grammar_in_json:
+                    tmlanguage = json.load(grammar_in_json)
+            except ValueError, e:
+                # Avoid DeprecationWarning by not calling e.message.
+                # sys.stderr.write("Error: '%s' %s" % (json_grammar, str(e)))
+                self.output_view.insert(edit, 0, "Error: '%s' %s" % (json_grammar, str(e)))
+            else:
+                target = os.path.join(path, grammar_name + '.tmLanguage')
+                self.output_view.insert(edit, 0, "Writing tmLanguage... (%s)" % target)
+                plistlib.writePlist(tmlanguage, target)
+
+        self.window.run_command("show_panel", {"panel": "output.aaa_package_dev"})
