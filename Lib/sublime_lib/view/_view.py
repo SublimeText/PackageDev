2c4c67d6bf69fd8d948e80291c2c0fea1ab71724
FichteFoll <fichtefoll2@googlemail.com>
2012-07-25 03:13:18 +0200

    Implement functions for region and coordinate handling into sublime_lib
    These are pretty useful when you want to extract a specific part of the
    buffer (like line 1 or the two last lines) and also checks the coordinates
    for validity unlike ``view.text_point()``
    (try ``print view.rowcol(view.text_point(0, 100000))`` and you'll see).

--- Lib/sublime_lib/view/_view.py
+++ Lib/sublime_lib/view/_view.py
@@ -1,4 +1,5 @@
 import contextlib
+from sublime import Region
 
 
 def append(view, text):
@@ -34,10 +35,92 @@ def has_file_ext(view, ext):
     """Returns ``True`` if view has file extension ``ext``.
     ``ext`` may be specified with or without leading ``.``.
     """
-    if not view.file_name(): return False
-    if not ext.strip().replace('.', ''): return False
-    
+    if not view.file_name() or not ext.strip().replace('.', ''):
+        return False
+
     if not ext.startswith('.'):
         ext = '.' + ext
 
     return view.file_name().endswith(ext)
+
+
+def rowcount(view):
+    """Returns the number of rows in ``view``.
+    """
+    return view.rowcol(view.size())[0] + 1
+
+
+def rowwidth(view, row):
+    """Returns the number of characters of ``row`` in ``view``.
+    """
+    return view.rowcol(view.line(view.text_point(row, 0)).end())[1]
+
+
+def relative_point(view, x=0, y=0, p=None):
+    """Returns a point (int) to the given coordinates.
+
+    Supports relative (negative) parameters and checks if they are in the
+    bounds (other than ``View.text_point()``).
+
+    If p (indexable -> ``p[0]``, ``len(p) == 2``; preferrably a touple) is
+    specified, x and y parameters are overridden.
+    """
+    if p is not None:
+        if len(p) != 2:
+            raise TypeError("Coordinates have 2 dimensions, not %d" % len(p))
+        (x, y) = p
+    row, col = x, y
+
+    # shortcut
+    if x == -1 and y == -1:
+        return view.size()
+
+    # calc absolute coords and check if coords are in the bounds
+    rowc = rowcount(view)
+    if x < 0:
+        row = max(rowc + x, 0)
+    else:
+        row = min(row, rowc)
+
+    roww = rowwidth(view, row)
+    if y < 0:
+        col = max(roww + y, 0)
+    else:
+        col = min(col, roww)
+
+    return view.text_point(row, col)
+
+
+def coorded_region(view, reg1=None, reg2=None):
+    """Returns a region of two coordinate pairs parsed by ``relative_point(view, p=reg1)``.
+
+    The pairs are supporsed to be indexable and have a length of 2.
+    Touples are preferred.
+
+    Defaults to the whole buffer (``reg1=(0, 0), reg2=(-1, -1)``).
+    """
+    reg1 = reg1 or (0, 0)
+    reg2 = reg2 or (-1, -1)
+
+    p1 = relative_point(view, p=reg1)
+    p2 = relative_point(view, p=reg2)
+    return Region(p1, p2)
+
+
+def coorded_substr(view, reg1=None, reg2=None):
+    """Returns the string of two coordinate pairs parsed by ``relative_point(view, p=reg1)``.
+
+    The pairs are supporsed to be indexable and have a length of 2.
+    Touples are preferred.
+
+    Defaults to the whole buffer.
+    """
+    return view.substr(coorded_region(view, reg1, reg2))
+
+
+def get_text(view):
+    """Returns the whole string of a buffer.
+
+    Alias for ``coorded_substr(view)``.
+    """
+    return coorded_substr(view)
