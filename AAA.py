ef9df3843fea8703c9d4bd1f037c90c4912d3492
FichteFoll <fichtefoll2@googlemail.com>
2012-07-31 02:05:16 +0200

    Patch AAA.py

--- AAA.py
+++ AAA.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 # Makes sublime_lib package available for all packages.
-if not os.path.join(sublime.packages_path(), "AAAPackageDev/Lib") in sys.path:
-    sys.path.append(os.path.join(sublime.packages_path(), "AAAPackageDev/Lib"))
+libpath = os.path.join(sublime.packages_path(), "AAAPackageDev", "Lib")
+if not libpath in sys.path:
+    sys.path.append(libpath)
     print "[AAAPackageDev] Added sublime_lib to sys.path."
