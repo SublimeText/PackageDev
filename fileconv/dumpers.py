6ec3992e30ad98616791d4a6d54c59530733c872
FichteFoll <fichtefoll2@googlemail.com>
2012-07-31 22:46:06 +0200

    Rework the validate_data logic
    Now it actually works.
    Also update the dumpers' parameters for _validate_data
    to work with even more different types.
    And add some documentation for yaml and json dumper
    parameters (plistlib has no options).

--- fileconv/dumpers.py
+++ fileconv/dumpers.py
@@ -73,8 +73,15 @@ class DumperProto(object):
 
     def validate_data(self, data, *args, **kwargs):
         """To be implemented (optional).
+
+            Must return the validated data object.
+
+            Example:
+                return self._validate_data(data, [
+                    ((lambda x: isinstance(x, float), int),
+                     (lambda x: isinstance(x, datetime.datetime), str))
+                ]
         """
-        # Some _validate_data call
         pass
 
     def _validate_data(self, data, funcs):
@@ -86,24 +93,41 @@ class DumperProto(object):
         the object to test.
 
         Example:
-            funcs = ((lamda x: isinstance(x, int), int)
-                     (lamda x: isinstance(x, str), str))
+            funcs = ((lambda x: isinstance(x, float), int),
+                     (lambda x: isinstance(x, datetime.datetime), str))
         """
         checked = []
 
         def check_recursive(obj):
+            if obj in checked:  # won't work for immutable types
+                return
+            checked.append(obj)
+
             for is_invalid, validate in funcs:
                 if is_invalid(obj):
                     obj = validate(obj)
-            if obj in checked:
-                return
-            if isinstance(obj, list) or isinstance(obj, dict):
-                for sub_obj in obj:
-                    checked.append(sub_obj)
-                    check_recursive(sub_obj)
 
-        check_recursive(data)
-        return data
+            if isinstance(obj, dict):  # dicts are fine
+                for key in obj:
+                    obj[key] = check_recursive(obj[key])
+
+            if isinstance(obj, list):  # lists are too
+                for i in range(len(obj)):
+                    obj[i] = check_recursive(obj[i])
+
+            if isinstance(obj, tuple):  # tuples are immutable ...
+                return tuple([check_recursive(sub_obj) for sub_obj in obj])
+
+            if isinstance(obj, set):  # sets ...
+                for val in obj:
+                    new_val = check_recursive(val)
+                    if new_val != val:  # a set's components are hashable, no need to "is"
+                        obj.remove(val)
+                        obj.add(new_val)
+
+            return obj
+
+        return check_recursive(data)
 
     def dump(self, data, *args, **kwargs):
         """Wraps the ``self.write`` function.
@@ -112,15 +136,16 @@ class DumperProto(object):
         """
         self.output.write_line("Writing %s... (%s)" % (self.name, self.new_file_path))
         self.output.show()
-        self.validate_data(data)
+        data = self.validate_data(data)
         try:
             self.write(data, *args, **kwargs)
         except Exception, e:
-            self.output.write_line("Error writing %s: %s" % (self.name, str(e)))
+            self.output.write_line("Error writing %s: %s" % (self.name, e))
+        else:
+            return True
 
     def write(self, data, *args, **kwargs):
-        """To be implemented.
-        """
+        """To be implemented."""
         pass
 
 
@@ -129,12 +154,65 @@ class JSONDumper(DumperProto):
     ext  = "json"
 
     def validate_data(self, data):
-        self._validate_data(data, [
-            (lambda x: isinstance(x, datetime.date), str)  # TOTEST # from yaml
-            (lambda x: isinstance(x, datetime.datetime), str)  # TOTEST # from plist and yaml
+        return self._validate_data(data, [
+            # TOTEST: sets
+            (lambda x: isinstance(x, plistlib.Data), lambda x: x.data),  # plist
+            (lambda x: isinstance(x, datetime.date), str),  # yaml
+            (lambda x: isinstance(x, datetime.datetime), str)  # plist and yaml
         ])
 
     def write(self, data, *args, **kwargs):
+        """Parameters:
+
+            skipkeys (bool)
+                Default: True
+
+                Dict keys that are not of a basic type (str, unicode, int,
+                long, float, bool, None) will be skipped instead of raising a
+                TypeError.
+
+            ensure_ascii (bool)
+                Default: True
+
+                If False, then some chunks may be unicode instances, subject to
+                normal Python str to unicode coercion rules.
+
+            check_circular (bool)
+                Default: False
+
+                If False, the circular reference check for container types will
+                be skipped and a circular reference will result in an
+                OverflowError (or worse).
+                Since we are working with file data here this is likely not
+                going to happen.
+
+            allow_nan (bool)
+                Default: True
+
+                If False, it will be a ValueError to serialize out of range
+                float values (nan, inf, -inf) in strict compliance of the JSON
+                specification, instead of using the JavaScript equivalents
+                (NaN, Infinity, -Infinity).
+
+            indent (int)
+                Default: 4
+
+                If a non-negative integer, then JSON array elements and object
+                members will be pretty-printed with that indent level. An
+                indent level of 0 will only insert newlines. None (the default)
+                selects the most compact representation.
+
+            separators (tuple, iterable)
+                Default: (', ', ': ')
+
+                (item_separator, dict_separator) tuple. (',', ':') is the most
+                compact JSON representation.
+
+            encoding (str)
+                Default: UTF-8
+
+                Character encoding for str instances, default is UTF-8.
+        """
         # Define default parameters
         json_params = dict(
                            skipkeys=True,
@@ -153,8 +231,10 @@ class PlistDumper(DumperProto):
     ext  = "plist"
 
     def validate_data(self, data):
-        self._validate_data(data, [
-            (lambda x: isinstance(x, datetime.date), str)  # TOTEST
+        return self._validate_data(data, [
+            # TOTEST: sets
+            # yaml; lost of precision when converting to datetime.datetime
+            (lambda x: isinstance(x, datetime.date), str)
         ])
 
     def write(self, data):
@@ -165,9 +245,74 @@ class YAMLDumper(DumperProto):
     name = "YAML"
     ext  = "yaml"
 
-    def write(self, data):
+    def validate_data(self, data):
+        return self._validate_data(data, [
+            # plistlib defines its own dict wrapper,
+            # yaml.safe_dump only dumps "dict" type ...
+            (lambda x: isinstance(x, plistlib._InternalDict), dict),
+            (lambda x: isinstance(x, plistlib.Data), lambda x: x.data)  # plist
+        ])
+
+    def write(self, data, *args, **kwargs):
+        """Parameters:
+
+            default_style (str)
+                Default: None
+                Accepted: None, '', '\'', '"', '|', '>'.
+
+                Indicates the style of the scalar.
+
+            default_flow_style (bool)
+                Default: True
+
+                Indicates if a collection is block or flow.
+
+            canonical (bool)
+                Default: None (-> False)
+
+                Export tag type to the output file.
+
+            indent (int)
+                Default: 2
+                Accepted: 1 < x < 10
+
+            width (int)
+                Default: 80
+                Accepted: > indent*2
+
+            allow_unicode (bool)
+                Default: None (-> False)
+
+            line_break (str)
+                Default: "\n"
+                Accepted: u'\r', u'\n', u'\r\n'
+
+            encoding (str)
+                Default: 'utf-8'
+
+            explicit_start (bool)
+                Default: None (-> False)
+
+                Explicit '---' at the start.
+
+            explicit_end (bool)
+                Default: None (-> False)
+
+                Excplicit '...' at the end.
+
+            version (tuple)
+                Default: Newest
+
+                Version of the YAML parser: tuple(major, minor).
+                Supports only major version 1.
+
+            tags (str?)
+                Default: None
+
+                ???
+        """
         with open(self.new_file_path, "w") as f:
-            yaml.safe_dump(data, f)
+            yaml.safe_dump(data, f, **kwargs)
 
 
 ###############################################################################
