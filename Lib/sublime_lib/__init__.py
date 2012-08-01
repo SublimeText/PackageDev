bba62bcc77afbf116b964b6c009a2e23322a5d48
FichteFoll <fichtefoll2@googlemail.com>
2012-08-01 15:08:34 +0200

    Update documentation for WindowAndTextCommand

--- Lib/sublime_lib/__init__.py
+++ Lib/sublime_lib/__init__.py
@@ -3,20 +3,29 @@ import sublime
 
 
 class WindowAndTextCommand(sublime_plugin.WindowCommand, sublime_plugin.TextCommand):
-    """A class to derive from when using a Window- and a TextCommand in one class
-    (e.g. when you make a build system that should/could also be called from the command
-    palette).
+    """A class to derive from when using a Window- and a TextCommand in one
+    class (e.g. when you make a build system that should/could also be called
+    from the command palette with the view in its focus).
 
         Defines both self.view and self.window.
 
-        Since this class derives from both Window- and a TextCommand it is also callable
-        with the known methods, like ``window.run_command("window_and_text")``.
-        I defined a dummy ``run`` method to prevent parameters from raising an exception so
-        this command call does exactly nothing.
-        Still a better method than having the parent class (the command you will define)
-        derive from three classes with the limitation that this class must be the first one
-        (the *Command classes do not use super() for multi-inheritance support; neither do I
-        but apparently I have reasons).
+        Be careful that self.window may be ``None`` when called as a
+        TextCommand because ``view.window()`` is not really safe and will
+        fail in quite a few cases. Since the compromise of using
+        ``sublime.active_window()`` in that case is not wanted by every
+        command I refused from doing so. Thus, the command's on duty to check
+        whether the window is valid.
+
+        Since this class derives from both Window- and a TextCommand it is also
+        callable with the known methods, like
+        ``window.run_command("window_and_text")``.
+        I defined a dummy ``run`` method to prevent parameters from raising an
+        exception so this command call does exactly nothing.
+        Still a better method than having the parent class (the command you
+        will define) derive from three classes with the limitation that this
+        class must be the first one (the *Command classes do not use super()
+        for multi-inheritance support; neither do I but apparently I have
+        reasons).
     """
     def __init__(self, param):
         # no super() call! this would get the references confused
