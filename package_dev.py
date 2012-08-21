0a3f0793bd1732a980108cee3a99fb76a0de2e11
FichteFoll <fichtefoll2@googlemail.com>
2012-08-21 16:42:23 +0200

    Remove ancient PlistToJson TextCommand from package_dev.py
    The is_enabled method did not consider cases when the file_name was ``None``.
    ... Why was that command even there?

--- package_dev.py
+++ package_dev.py
@@ -1,10 +1,6 @@
 import sublime
 import sublime_plugin
 
-import plistlib
-import json
-
-import glob
 import os
 import sys
 
@@ -59,17 +55,16 @@ class NewPackageCommand(sublime_plugin.WindowCommand):
 
         pam.create_new(pkg_name)
 
-    
     def on_cancel(self):
         status('on_cancel')
-    
+
     def on_changed(self):
         status('on_changed')
 
     def run(self):
         self.window.show_input_panel(
                             "New Package Name", '', self.on_done, None, None)
-                            
+
 
 class DeletePackageCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -78,22 +73,22 @@ class DeletePackageCommand(sublime_plugin.WindowCommand):
 
 
 class PackageManager(object):
-    
+
     def is_installed(self, name):
         raise NotImplemented
-    
+
     def exists(self, name):
         return path_exists(root_at_packages(name))
-    
+
     def browse(self):
         # Let user choose.
-        sublime.active_window().run_command("open_dir", 
+        sublime.active_window().run_command("open_dir",
                                             {"dir": sublime.packages_path()})
-    
+
     def create_new(self, name):
         print "[NewPackage] Creating new package...",
         print root_at_packages(name)
-        
+
         if self.dry_run:
             msg = "[NewPackage] ** Nothing done. This was a test. **"
             print msg
@@ -102,7 +97,7 @@ class PackageManager(object):
 
         # Create top folder, default folders, default files.
         map(os.makedirs, [root_at_packages(name, d) for d in DEFAULT_DIRS])
-        
+
         for f, template in [(root_at_packages(name, fname), template)
                                         for fname, template in DEFAULT_FILES]:
             with open(f, 'w') as fh:
@@ -116,21 +111,10 @@ class PackageManager(object):
                         content = "".join(open(template, 'r').readlines())
 
                     fh.write(content)
-        
+
         msg = "[NewPackage] Created new package '%s'." % name
         print msg
         status(msg)
-    
+
     def __init__(self, dry_run=False):
         self.dry_run = dry_run
-
-
-class PlistToJson(sublime_plugin.TextCommand):
-    def is_enabled(self ):
-        return self.view.file_name().endswith('.tmLanguage')
-        
-    def run(self, edit):
-        plist_data = plistlib.readPlist(self.view.file_name())
-        v = self.view.window().new_file()
-        v.insert(edit, 0, json.dumps(plist_data, indent=4))
-        v.set_syntax_file('Packages/AAAPackageDev/Support/Sublime JSON Syntax Definition.tmLanguage')
