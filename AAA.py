c33debf8a23b125fffc9cb3a497293ec8089f078
Dan Cowell <dan@cloudcoders.com.au>
2013-10-26 15:09:15 +1000

    Fixes incorrect resolution of Lib directory when packages directory is a symlink.

--- AAA.py
+++ AAA.py
@@ -3,10 +3,10 @@ import sys
 
 from sublime import packages_path
 
-PLUGIN_NAME = os.getcwdu().replace(packages_path(), '')[1:]
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Makes sublime_lib package available for all packages.
-libpath = os.path.join(packages_path(), PLUGIN_NAME, "Lib")
+libpath = os.path.join(BASE_PATH, "Lib")
 if not libpath in sys.path:
     sys.path.append(libpath)
     print "[AAAPackageDev] Added sublime_lib to sys.path."
