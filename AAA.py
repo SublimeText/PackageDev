67ca7ddf3d573b8ea47b3001f53edd835c69a256
simonzack <simonzack@email.com>
2013-12-30 02:52:31 +1100

    import fixes, previous commit broke sublime text 2

--- AAA.py
+++ AAA.py
@@ -1,3 +1,19 @@
+import os
+import sys
+
+# Makes sublime_lib package available for all packages.
+PLUGIN_DIR = os.path.abspath(os.path.dirname(__file__))
+PLUGIN_NAME = os.path.split(PLUGIN_DIR)[1]
+
+libpaths = [
+    os.path.join(PLUGIN_DIR, "Lib"),
+    # The current package is not imported as a python module in sublime text 2, so add the import path and use absolute imports.
+    os.path.dirname(PLUGIN_DIR)
+]
+for libpath in libpaths:
+    if not libpath in sys.path:
+        sys.path.append(libpath)
+        print("[AAAPackageDev] Added {0} to sys.path.".format(libpath))
 
 # Import compatibility fixes.
 import AAAPackageDev.py_compat
