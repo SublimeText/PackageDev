500178afed9931b5080364d483ae8893f3ffda8c
FichteFoll <fichtefoll2@googlemail.com>
2017-08-20 23:32:31 +0200

    Rename to include "Text"

--- plugins_/lib/syntax_paths.py
+++ plugins_/lib/syntax_paths.py
@@ -18,7 +18,7 @@ def _build_path(sub_package_name, file_name=None, textmate=False):
 # paths to our package
 PLIST              = _build_path("Property List")
 BUILD_SYSTEM       = _build_path("Sublime Text Build System", textmate=True)
-COMMANDS           = _build_path("Sublime Text Commands", "Sublime Commands", textmate=True)
+COMMANDS           = _build_path("Sublime Text Commands")
 COMPLETIONS        = _build_path("Sublime Text Completions")
 KEYMAP             = _build_path("Sublime Text Keymap")
 MACROS             = _build_path("Sublime Text Macros", textmate=True)
