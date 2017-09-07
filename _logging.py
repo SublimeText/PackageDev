24445eee5611034c19210cc487eb077603ed93b7
FichteFoll <fichtefoll2@googlemail.com>
2017-09-07 20:11:35 +0200

    Reduce "logging level change" event level to info
    But ensure it is getting seen if the new level is equal to or below info
    and the current level is not.
    That way it gets seen when the level is set to info or below and when it
    is set higher again.
    
    Closes #134.

--- _logging.py
+++ _logging.py
@@ -5,7 +5,7 @@ import sublime
 
 DEFAULT_LOG_LEVEL = logging.WARNING
 DEFAULT_LOG_LEVEL_NAME = logging.getLevelName(DEFAULT_LOG_LEVEL)
-
+EVENT_LEVEL = logging.INFO
 
 pl = logging.getLogger(__package__)
 handler = logging.StreamHandler()
@@ -23,13 +23,19 @@ def _settings():
 
 def plugin_loaded():
     def on_settings_reload():
-        cur_log_level_name = logging.getLevelName(pl.getEffectiveLevel())
+        cur_log_level = pl.getEffectiveLevel()
+        cur_log_level_name = logging.getLevelName(cur_log_level)
         new_log_level_name = _settings().get('log_level', DEFAULT_LOG_LEVEL_NAME).upper()
-        log_level = getattr(logging, new_log_level_name, DEFAULT_LOG_LEVEL)
+        new_log_level = getattr(logging, new_log_level_name, DEFAULT_LOG_LEVEL)
 
         if new_log_level_name != cur_log_level_name:
-            l.warning("Changing log level from %r to %r", cur_log_level_name, new_log_level_name)
-            pl.setLevel(log_level)
+            if cur_log_level > EVENT_LEVEL and new_log_level <= EVENT_LEVEL:
+                # Only set level before emitting log event if it would not be seen otherwise
+                pl.setLevel(new_log_level)
+            l.log(EVENT_LEVEL,
+                  "Changing log level from %r to %r",
+                  cur_log_level_name, new_log_level_name)
+            pl.setLevel(new_log_level)  # Just set it again to be sure
 
     _settings().add_on_change(__name__, on_settings_reload)
     on_settings_reload()  # trigger on inital settings load, too
