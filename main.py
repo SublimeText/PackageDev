94e0e31a8329cbbdc1545fa5c12b04600422627f
FichteFoll <fichtefoll2@googlemail.com>
2017-09-20 17:10:07 +0200

    Add code to remove cached sub-modules on upgrade
    This is untested because I'm too lazy to simulate a package updating
    situation, but I generally believe it should work. It shouldn't break
    anything, at least.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+try:
+    from package_control import events
+except ImportError:
+    pass
+else:
+    if events.post_upgrade(__package__):
+        # clean up sys.modules to ensure all submodules are reloaded
+        import sys
+        modules_to_clear = set()
+        for module_name in sys.modules:
+            if module_name.startswith(__package__):
+                modules_to_clear.add(module_name)
+
+        print("[{}] Cleaning up {} cached modules after update…"
+              .format(__package__, len(modules_to_clear)))
+        for module_name in modules_to_clear:
+            del sys.modules[module_name]
+
 # Must be named "plugins_"
 # because sublime_plugin claims a plugin module's `plugin` attribute for itself.
 
