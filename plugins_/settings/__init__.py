eb8b1e97c74e5c9616164abaa02dbd81e4062e1d
DeathAxe <deathaxe@web.de>
2017-10-08 12:20:44 +0200

    Enhance settings tooltip visibility for light color schemes
    The CSS of the settings tooltips always blended the --background with white,
    which makes in invisible on white bacgkound as Windows does not render a shadow
    around tooltips. In order to fix that issue, this commit makes use of ST's
    `html.light` and `html.dark` classes to define different colors for light and
    dark color schemes.

--- plugins_/settings/__init__.py
+++ plugins_/settings/__init__.py
@@ -21,22 +21,32 @@ __all__ = (
 POPUP_TEMPLATE = """
 <body id="sublime-settings">
 <style>
-    body {{
+    html.light {{
+        --html-background: color(var(--background) blend(black 91%));
+        --border-color: color(var(--html-background) blend(black 95%));
+    }}
+    html.dark {{
+        --html-background: color(var(--background) blend(white 93%));
+        --border-color: color(var(--html-background) blend(white 95%));
+    }}
+    html, body {{
         margin: 0;
         padding: 0;
+        background-color: var(--html-background);
+        color: var(--foreground);
     }}
     h1, h2 {{
-        border-bottom: 1px solid color(var(--background) blend(white 80%));
+        border-bottom: 1px solid var(--border-color);
         font-weight: normal;
         margin: 0;
         padding: 0.5rem 0.6rem;
     }}
     h1 {{
-        color: color(var(--background) blend(var(--orangish) 20%));
+        color: var(--orangish);
         font-size: 1.0rem;
     }}
     h2 {{
-        color: color(var(--background) blend(var(--foreground) 30%));
+        color: color(var(--html-background) blend(var(--foreground) 30%));
         font-size: 1.0rem;
         font-family: monospace;
     }}
