795ea827b7653ee7955c78d4717eeab691d57dc1
FichteFoll <fichtefoll2@googlemail.com>
2018-10-23 02:24:06 +0200

    Provide variable completions for themes

--- plugins_/color_scheme_dev.py
+++ plugins_/color_scheme_dev.py
@@ -28,9 +28,17 @@ def _inhibit_word_completions(func):
 
 class ColorSchemeCompletionsListener(sublime_plugin.ViewEventListener):
 
+    """Provide variable and scope name completions for color schemes.
+
+    Extract completions from defined variables in the current file
+    and determine scope completions based on our scope_data module.
+
+    Also provide variable completions for themes.
+    """
+
     @classmethod
     def is_applicable(cls, settings):
-        return settings.get('syntax') == syntax_paths.COLOR_SCHEME
+        return settings.get('syntax') in (syntax_paths.COLOR_SCHEME, syntax_paths.THEME)
 
     def _line_prefix(self, point):
         _, col = self.view.rowcol(point)
@@ -38,7 +46,8 @@ class ColorSchemeCompletionsListener(sublime_plugin.ViewEventListener):
         return line[:col]
 
     def variable_completions(self, prefix, locations):
-        variable_regions = self.view.find_by_selector("entity.name.variable.sublime-color-scheme")
+        variable_regions = self.view.find_by_selector("entity.name.variable.sublime-color-scheme, "
+                                                      "entity.name.variable.sublime-theme")
         variables = set(self.view.substr(r) for r in variable_regions)
         l.debug("Found %d variables to complete: %r", len(variables), sorted(variables))
         return sorted(("{}\tvariable".format(var), var) for var in variables)
