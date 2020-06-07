58c91dbc7b73f582dd28750f95db53478c53cad4
FichteFoll <fichtefoll2@googlemail.com>
2020-06-07 21:55:23 +0200

    Cause CI failures on purpose

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,7 +11,7 @@ from .snippet_dev import *  # noqa
 from .syntax_dev import *  # noqa
 from .syntax_dev_legacy import *  # noqa
 from .syntaxtest_dev import *  # noqa
-from .theme_dev import *  # noqa
+from .theme_dev import *
 
 from . import xml_hidden_extensions_hotfix
 
