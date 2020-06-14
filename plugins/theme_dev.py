90347aa47113e39ae5086449471e2740f294d1eb
FichteFoll <fichtefoll2@googlemail.com>
2020-06-14 15:53:37 +0200

    Theme: Provide completions for "extends" key

--- plugins/theme_dev.py
+++ plugins/theme_dev.py
@@ -1,10 +1,16 @@
+import logging
+
 import sublime
 import sublime_plugin
 
 from sublime_lib import ResourcePath
 
+from .lib import syntax_paths
+from .color_scheme_dev import inhibit_word_completions
+
 __all__ = (
     'PackagedevEditThemeCommand',
+    'ThemeCompletionsListener',
 )
 
 THEME_TEMPLATE = """\
@@ -16,6 +22,8 @@ THEME_TEMPLATE = """\
   "rules": [
   ],
 }""".replace("  ", "\t")
+
+logger = logging.getLogger(__name__)
 
 
 class PackagedevEditThemeCommand(sublime_plugin.WindowCommand):
@@ -33,3 +41,42 @@ class PackagedevEditThemeCommand(sublime_plugin.WindowCommand):
                 "default": THEME_TEMPLATE,
             },
         )
+
+
+class ThemeCompletionsListener(sublime_plugin.ViewEventListener):
+
+    """Provide completions for themes.
+
+    Offer names of theme files to extend.
+    Other completions are shared with color schemes
+    and implemented in color_scheme_dev.py.
+    """
+
+    @classmethod
+    def applies_to_primary_view_only(cls):
+        return False
+
+    @classmethod
+    def is_applicable(cls, settings):
+        return settings.get('syntax') in (syntax_paths.COLOR_SCHEME, syntax_paths.THEME)
+
+    @inhibit_word_completions
+    def on_query_completions(self, prefix, locations):
+        if len(locations) != 1:
+            return None
+        point = locations[0]
+
+        if self.view.match_selector(point, "meta.extends.sublime-theme"):
+            return self.extends_completions()
+
+    def extends_completions(self):
+        names = {res.rsplit("/", 1)[-1] for res in sublime.find_resources("*.sublime-theme")}
+        try:
+            this_resource = ResourcePath.from_file_path(self.view.file_name())
+        except TypeError:
+            pass
+        else:
+            names -= {this_resource.name}
+        sorted_names = sorted(names)
+        logger.debug("Found %d themes to complete: %r", len(names), sorted_names)
+        return [("{}\ttheme".format(name), name) for name in sorted_names]
